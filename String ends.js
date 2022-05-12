14e5efecf05c964732825b5bf2cb041e43500000
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-05-12 17:50:15 +0300

    Create String ends.js

---
+++ String ends.js
@@ -0,0 +1,17 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+function solution(str, ending) {
+              // TODO: complete
+
+              return ending.length < 1 ? true : (str.slice(-ending.length) === ending);
+}
+
+//ending.length < 1 ? true will solve the incidence where the ending string is null
+//also the ending of str is null
