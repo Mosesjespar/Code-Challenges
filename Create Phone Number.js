29277fa29d17bc8e07427c37bb037677cf1074b8
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-05-15 17:05:32 +0300

    Added an alternative

--- Create Phone Number.js
+++ Create Phone Number.js
@@ -13,3 +13,18 @@ createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
     
     return format;
   }
+
+  //        OR
+  /*
+  function createPhoneNumber(numbers){
+  
+  if(numbers.length<10 || numbers.length>10){
+      return ""
+  }
+  
+  else{
+   return "("+numbers[0]+numbers[1]+numbers[2]+")" + " " +numbers[3]+numbers[4]+numbers[5]
+    +"-"+numbers[6]+numbers[7]+numbers[8]+numbers[9]
+  }
+}
+  */
