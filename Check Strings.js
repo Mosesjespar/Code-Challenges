5d42902965980b32e4c22c8d32b645258ed5653e
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-05-17 17:52:23 +0300

    Added the task

--- Check Strings.js
+++ Check Strings.js
@@ -1,3 +1,8 @@
+/*
+    Write a function that takes two strings s1 and s2 as parameters.The function should return true if the characters in s1 can be arranged
+    to form s2 otherwise return false
+*/
+
 function checkStrings(s1, s2) {
     if (s1.length < s2.length) {
         return false
