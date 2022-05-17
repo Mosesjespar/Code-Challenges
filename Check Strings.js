f44e6cf7e1db2a80d584ec28ce9740bb1a8fb917
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-05-17 17:48:28 +0300

    Create Check Strings.js

---
+++ Check Strings.js
@@ -0,0 +1,16 @@
+function checkStrings(s1, s2) {
+    if (s1.length < s2.length) {
+        return false
+    }
+    for (var i = 0; i < s2.length; i++) {
+
+
+        return s1.split(s2[i]).length >= s2.split(s2[i]).length
+
+
+    }
+
+
+
+}
+
