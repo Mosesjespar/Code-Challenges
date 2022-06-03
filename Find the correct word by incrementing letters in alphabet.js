08a557ceb141d8f98ee513f412f9baad5a4c686a
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-03 22:52:46 +0300

    Create Find the correct word by incrementing letters in alphabet.js

---
+++ Find the correct word by incrementing letters in alphabet.js
@@ -0,0 +1,20 @@
+// Write a function that takes a string as argument
+// As it is, the string has no meaning
+// Increment each letter to the next letter in the alphabet
+// Return the correct word
+/*Test Cases:
+myFunction('bnchmf')
+Expected
+'coding'
+myFunction('bgddrd')
+Expected
+'cheese'
+myFunction('sdrshmf')
+Expected
+'testing'
+ */
+function myFunction(str) {
+    const arr = [...str];
+    const correctedArray = arr.map(e => String.fromCharCode(e.charCodeAt() + 1));
+    return correctedArray.join('');
+}
