fc9d36c4f42248e2e684464ba0f5509d19bf90be
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-10 19:45:57 +0300

    Update Who likes it.js

--- Who likes it.js
+++ Who likes it.js
@@ -1,5 +1,6 @@
 /*
-You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
+We want to create the text that should be displayed next to such an item.
 
 Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 
@@ -9,27 +10,19 @@ Implement the function which takes an array containing the names of people that 
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 */
 
-
 function likes(names) {
     // TODO
     let x = names.length;
     if (x < 1) {
         return 'no one likes this'
-
     }
     else if (x === 1) {
         return names[0] + ' likes this'
-
     }
     else if (x === 2) { return names[0] + ' and ' + names[1] + ' like this' }
     else if (x === 3) { return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this' }
-
     else if (x >= 4) {
-
         return names[0] + ', ' + names[1] + ' and ' + (x - 2) + ' others like this'
-
     }
-
-
 }
 
